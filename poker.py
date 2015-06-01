e6443683340c67b0d2d795671f3d8c48fe54965e
Antoine Dahan <adahan93@gmail.com>
2015-06-01 10:31:35 -0400

    huge simplification, now just recording attributes of the hand, will compare later

--- poker.py
+++ poker.py
@@ -62,6 +62,24 @@ def get_card_distribution(hand_cards):
 
     return sorted(card_distribution)
 
+def is_flush(hand_dist):
+
+    # doesn't have 5 unique values, so can't be a flush
+    if(len(hand_dist) != 5):
+        return False
+
+    else:
+        flushes_suit = hand_dist[0][1][0]
+        is_hand_flush = True # asume flush until different suit seen
+
+        # check to make sure all other cards in hand of same suit
+        for card in hand_dist:
+            if card[1][0] != flushes_suit:
+                is_hand_flush = False
+                break
+
+        return is_hand_flush
+
 
 # determine whether the given and distribution is a straight
 def is_straight(hand_dist):
@@ -81,73 +99,73 @@ def is_straight(hand_dist):
     else:
         return False
 
+# get the value of the top card of a straight
+# assumes the hand distribution input represents a straight
+def get_straight_top_value(hand_dist):
+
+    hand_dist = sorted(hand_dist)
+    straight_high_value = 0
+    if(hand_dist[len(hand_dist) - 1][0] == 14 
+        and hand_dist[len(hand_dist) - 2][0] != 13):
+        return hand_dist[len(hand_dist) - 2][0]
+    else:
+        return hand_dist[len(hand_dist) - 1][0]
+
 # Get the maximum straight(s) by value out of the straights whose index in the 
 # hands_distributions list are specified in the straight_indexes list
-def get_max_straights(straight_indexes, hands_distributions):
-    max_straights = list()
-    for straight_index in straight_indexes:
-        this_straight = sorted(hands_distributions[straight_index])
+# def get_max_straights(straight_indexes, hands_distributions):
+#     max_straights = list()
+#     for straight_index in straight_indexes:
+#         this_straight = sorted(hands_distributions[straight_index])
 
-        # get the high value of the straight (if Ace is highest but used as "1" then 
-        # record the second highest value (will be the 5))
-        straight_high_value = 0
-        if(this_straight[len(this_straight) - 1][0] == 14 
-            and this_straight[len(this_straight) - 2][0] != 13):
-            straight_high_value = this_straight[len(this_straight) - 2][0]
-        else:
-            straight_high_value = this_straight[len(this_straight) - 1][0]
+#         straight_high_value = get_straight_top_value(this_straight)
 
-        # if max straights is empty or the high value of this straight is greater than 
-        # the current highest straight's value, then clear the max_straights list and 
-        # append the new max straight's index
-        if(len(max_straights) == 0 or max_straights[0][0] < straight_high_value):
-            max_straights = list()
-            max_straights.append([straight_high_value, straight_index])
+#         # if max straights is empty or the high value of this straight is greater than 
+#         # the current highest straight's value, then clear the max_straights list and 
+#         # append the new max straight's index
+#         if(len(max_straights) == 0 or max_straights[0][0] < straight_high_value):
+#             max_straights = list()
+#             max_straights.append([straight_high_value, straight_index])
 
-        # if this straight's high value is equal to the current max straight's high
-        # value, then append this straight it to the max_straights list
-        elif max_straights[0][0] == straight_high_value:
-            max_straights.append([straight_high_value, straight_index])
+#         # if this straight's high value is equal to the current max straight's high
+#         # value, then append this straight it to the max_straights list
+#         elif max_straights[0][0] == straight_high_value:
+#             max_straights.append([straight_high_value, straight_index])
 
 
-    return_list_straights = list()
+#     return_list_straights = list()
 
-    for max_straight in max_straights:
-        return_list_straights.append(max_straight[1])
+#     for max_straight in max_straights:
+#         return_list_straights.append(max_straight[1])
 
-    return return_list_straights
+#     return return_list_straights
 
 # Returns the a list of indexes of the hands in hands_distributions with the largest 
 # value (if returned list length > 1, then there is a tie between 1 or more hands)
-def hand_comparison(hands_distributions):
+#def hand_comparison(hands_distributions):
 
-    # stores the straights indexes in hands_distributions
-    straights_indexes = list()
-    for i in range(len(hands_distributions)):
-        if is_straight(hands_distributions[i]):
-            straights_indexes.append(i)
 
-    # check for straight flush
-    straight_flushes_indexes = list()
+def get_hand_attributes(hand_dist):
 
-    for straight_index in straights_indexes:
-        this_straight = hands_distributions[straight_index]
-        flushes_suit = this_straight[0][1][0]
-        is_straight_flush = True
+    # stores the attributes of the hand (flush, straight, 2-pair, etc.)
+    hand_attributes = list()
 
-        for card_index in range(1, len(this_straight)):
-            if this_straight[card_index][1][0] != flushes_suit:
-                is_straight_flush = False
-                break
+    ##################################
+    # check the hand for a straight flushe
+    if is_straight(hand_dist) and is_flush(hand_dist):
+        top_value = get_straight_top_value(hand_dist)
+        hand_attributes.append([8, top_value])
+    ##################################
 
-        if is_straight_flush:
-            straight_flushes_indexes.append(straight_index)
 
-    # if at least one straight flush exists, then return 
-    # the one(s) with the greatest value 
-    if len(straight_flushes_indexes) > 0:
-        max_straight_flushes_indexes = get_max_straights(straight_flushes_indexes, hands_distributions)            
-        return max_straight_flushes_indexes
+    ##################################
+    # check the hand for 4 of a kind
+    for value_tally in hand_dist:
+        if(len(value_tally[1]) == 4):
+            hand_attributes.append([7, value_tally[0]])
+    ##################################
+
+    return hand_attributes
 
 deck = setup_deck()
 
@@ -161,9 +179,12 @@ current_state = deal(get_num_players(), deck)
 hand_a = [[13, "C"], [11, "C"], [12, "C"], [9, "C"], [10, "C"]]
 hand_b = [[13, "D"], [11, "D"], [12, "D"], [9, "D"], [10, "D"]]
 hand_c = [[13, "H"], [11, "H"], [12, "H"], [9, "H"], [10, "H"]]
-hand_d = [[14, "S"], [2, "S"], [3, "S"], [4, "S"], [5, "S"]]
+hand_d = [[2, "H"], [3, "H"], [5, "H"], [14, "H"], [4, "H"]]
+hand_d = [[2, "H"], [2, "C"], [2, "D"], [2, "S"], [4, "H"]]
 
-print(hand_comparison([get_card_distribution(hand_a), get_card_distribution(hand_b), get_card_distribution(hand_c), get_card_distribution(hand_d)]))
+
+print(get_hand_attributes(get_card_distribution(hand_d)))
+#print(hand_comparison([get_card_distribution(hand_a), get_card_distribution(hand_b), get_card_distribution(hand_c), get_card_distribution(hand_d)]))
 #print(get_hand_values(hand_cards))
 
 #hand_comparison()
